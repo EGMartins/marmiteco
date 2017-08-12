bf07864ec355389d4d39a6cb6afafc961becf251
Eduardo <mirassol.code@gmail.com>
2017-08-12 16:44:17 -0300

    Styling the home page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,86 @@
 $(document).ready(function(){
   $('.parallax').parallax();
 });
+
+$(document).ready(function(){
+ 
+  // hide our element on page load
+  $('#section01').css('opacity', 0);
+ 
+  $('#section01').waypoint(function() {
+      $('#section01').addClass('fadeInLeft');
+  }, { offset: '70%' });
+ 
+});
+
+$(document).ready(function(){
+ 
+  // hide our element on page load
+  $('#section02').css('opacity', 0);
+ 
+  $('#section02').waypoint(function() {
+      $('#section02').addClass('fadeInRight');
+  }, { offset: '80%' });
+ 
+});
+
+$(document).ready(function(){
+ 
+  // hide our element on page load
+  $('#section03').css('opacity', 0);
+ 
+  $('#section03').waypoint(function() {
+      $('#section03').addClass('fadeInRight');
+  }, { offset: '70%' });
+ 
+});
+
+$(document).ready(function(){
+ 
+  // hide our element on page load
+  $('#section04').css('opacity', 0);
+ 
+  $('#section04').waypoint(function() {
+      $('#section04').addClass('fadeInLeft');
+  }, { offset: '80%' });
+ 
+});
+
+$(document).ready(function(){
+ 
+  // hide our element on page load
+  $('#my-card').css('opacity', 0);
+ 
+  $('#my-card').waypoint(function() {
+      $('#my-card').addClass('zoomInUp');
+      $('#my-card').css('opacity', 100);
+  }, { offset: '90%' });
+  
+ 
+});
+
+$(document).ready(function(){
+ 
+  // hide our element on page load
+  $('#my-card02').css('opacity', 0);
+ 
+  $('#my-card02').waypoint(function() {
+      $('#my-card02').addClass('zoomInUp');
+      $('#my-card02').css('opacity', 100);
+  }, { offset: '90%' });
+  
+ 
+});
+
+$(document).ready(function(){
+ 
+  // hide our element on page load
+  $('#my-card03').css('opacity', 0);
+ 
+  $('#my-card03').waypoint(function() {
+      $('#my-card03').addClass('zoomInUp');
+      $('#my-card03').css('opacity', 100);
+  }, { offset: '90%' });
+  
+ 
+});
