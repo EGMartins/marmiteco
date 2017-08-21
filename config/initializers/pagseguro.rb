17c831fb3d33f1604f4015852ea3277085cc559a
Eduardo <mirassol.code@gmail.com>
2017-08-21 20:50:35 -0300

    Adjusting production PagSeguro

--- config/initializers/pagseguro.rb
+++ config/initializers/pagseguro.rb
@@ -1,6 +1,8 @@
 PagSeguro.configure do |config|
-	config.token = "6C2023E0E5874B37A5521EA041930691"
-	# config.token = "592DC1763C3C51955483FFB7223B4048"
+	# Sandbox
+	# config.token = "6C2023E0E5874B37A5521EA041930691"
+	# Production
+	config.token = "BE23540ADB144CEF8B6432C766D5A701"
 	config.email = "mirassol.code@gmail.com"
 	config.environment = :sandbox # ou :sandbox. O padrão é production.
 	config.encoding = "UTF-8"
