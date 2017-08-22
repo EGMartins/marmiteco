297cc40f988543e375f7f0fa14234fb7ee426da1
Eduardo <mirassol.code@gmail.com>
2017-08-22 01:26:29 -0300

    Change payment mode

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -14,7 +14,7 @@ class OrderController < ApplicationController
 
     payment = PagSeguro::CreditCardTransactionRequest.new
     payment.notification_url = "https://safe-dusk-38202.herokuapp.com/notification"
-    payment.payment_mode = "gateway"
+    payment.payment_mode = "creditCard"
 
     # Aqui vão os itens que serão cobrados na transação, caso você tenha multiplos itens
     # em um carrinho altere aqui para incluir sua lista de itens
