a69b9e8e7122e5de40aa75d326e43a95799515b5
Eduardo <mirassol.code@gmail.com>
2017-09-10 02:23:39 -0300

    Changing PRY requirement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include ApplicationHelper
   helper_method :current_user
-  require 'pry'
   
   NotAuthenticated = Class.new(StandardError)
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
